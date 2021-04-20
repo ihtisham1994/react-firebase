389dd593896b5281dfdc3ad2fc31fd484c6f86f8
ihtisham1994 <ihtisham.anwar@invozone.com>
2021-04-20 18:32:24 +0500

    Signup has been completed using firebase with react and tailwind ui

--- src/App.js
+++ src/App.js
@@ -6,29 +6,32 @@ import Signin from "./components/Signin";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
+import AuthProvider from "./contexts/AuthContext";
 
 
 function App() {
 
   return (
-      <Router>
-          <div className="App">
-              <Navbar/>
-              <div className="content">
-                  <Switch>
-                      <Route exact path="/">
-                          <Home/>
-                      </Route>
-                      <Route path="/signin">
-                          <Signin/>
-                      </Route>
-                      <Route path="/register">
-                          <Signup/>
-                      </Route>
-                  </Switch>
+      <AuthProvider>
+          <Router>
+              <div className="App">
+                  <Navbar/>
+                  <div className="content">
+                      <Switch>
+                          <Route exact path="/">
+                              <Home/>
+                          </Route>
+                          <Route path="/signin">
+                              <Signin/>
+                          </Route>
+                          <Route path="/register">
+                              <Signup/>
+                          </Route>
+                      </Switch>
+                  </div>
               </div>
-          </div>
-      </Router>
+          </Router>
+      </AuthProvider>
   );
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,19 +1,37 @@
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 import {Link} from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 
 const Signup = () => {
 
     const emailRef = useRef();
     const passwordRef = useRef();
+    const { signup, currentUser } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
-    const getFormData = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("Email--->>>", emailRef.current.value, "Password--->>>", passwordRef.current.value );
+
+        /*if ( passwordRef.current.value !== passwordConfirmRef.current.value ){
+            return setError('Passwords do not match');
+        }*/
+
+        try{
+            setError('');
+            setLoading(true);
+            await signup( emailRef.current.value, passwordRef.current.value );
+            // await signup( emailRef.current.value );
+        } catch (e) {
+            setError('failed to create an account')
+        }
+        setLoading(false);
     };
 
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            { JSON.stringify( currentUser )}
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <img
                     className="mx-auto h-12 w-auto"
@@ -31,7 +49,20 @@ const Signup = () => {
 
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <form className="space-y-6" action="#" method="POST">
+                    {error &&
+                    <div className="rounded-md bg-red-50 p-4">
+                        <div className="flex">
+                            <div className="ml-3">
+                                <div className="mt-2 text-sm text-red-700">
+                                    <ul className="list-disc pl-5 space-y-1">
+                                        <li>{ error }</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    }
+                    <form className="space-y-6" onSubmit={handleSubmit} method="POST">
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                                 Email address
@@ -66,7 +97,8 @@ const Signup = () => {
                             </div>
                         </div>
                         <div>
-                            <button onClick={getFormData}
+                            <button
+                                disabled={loading}
                                 type="submit"
                                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
